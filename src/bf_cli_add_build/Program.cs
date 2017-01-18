0c78dff6f3c1e731df698d4ecaa917a3363f0821
pmnxis <pmnxis@gmail.com>
2017-01-18 14:41:29 +0900

    update

--- src/bf_cli_add_build/Program.cs
+++ src/bf_cli_add_build/Program.cs
@@ -2,8 +2,12 @@
 
 using BuildsFlightCore;
 
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
 // USAGE
-//    bf_cli_add_build APP_NAME VERSION URL NOTE
+//    bf_cli_add_build APP_NAME VERSION FILEPATH NOTE
 namespace bf_cli_add_build
 {
 	class MainClass
@@ -15,13 +19,13 @@ namespace bf_cli_add_build
 			if (args.Length < 4)
 			{
 				Console.WriteLine("Usage:");
-				Console.WriteLine("   bf_cli_add_build APP_NAME VERSION URL NOTE");
+				Console.WriteLine("   bf_cli_add_build APP_NAME VERSION FILEPATH NOTE");
 				return -1;
 			}
 
 			var appName = args[0];
 			var version = args[1];
-			var url = args[2];
+			var filepath = args[2];
 			var note = args[3];
 
 			var app = BuildsFlight.GetApp(appName);
@@ -31,7 +35,30 @@ namespace bf_cli_add_build
 				return -1;
 			}
 
-			app.AddBuild(version, url, note);
+            var s3 = new AmazonS3Client(
+                CliBase.AccessKey, CliBase.AccessSecret,
+                RegionEndpoint.GetBySystemName(CliBase.RegionName));
+            var s3Key = $"buildsflight/{appName}/{version}/package.zip";
+
+            var resp = s3.PutObject(new PutObjectRequest()
+            {
+                FilePath = filepath,
+                Key = s3Key,
+                CannedACL = S3CannedACL.PublicRead,
+                BucketName = CliBase.BucketName
+            });
+            if (resp.HttpStatusCode != System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine("Failed to upload file.");
+                return -1;
+            }
+
+            var s3FilePath =
+                $"https://s3.{CliBase.RegionName}.amazonaws.com/{CliBase.BucketName}/{s3Key}";
+
+            Console.WriteLine(s3FilePath);
+
+			app.AddBuild(version, s3FilePath, note);
 
 			return 0;
 		}
--- src/bf_cli_base/CliBase.cs
+++ src/bf_cli_base/CliBase.cs
@@ -5,26 +5,31 @@ using BuildsFlightCore;
 
 public class CliBase
 {
+    public static string AccessKey;
+    public static string AccessSecret;
+    public static string RegionName;
+    public static string BucketName;
+
 	public static void Init()
 	{
         var target = EnvironmentVariableTarget.User | EnvironmentVariableTarget.Process;
 
-		var appKey = Environment.GetEnvironmentVariable("BF_ACCESS_KEY", target);
-		var appSecret = Environment.GetEnvironmentVariable("BF_ACCESS_SECRET", target);
-		var region = Environment.GetEnvironmentVariable("BF_REGION", target) ?? "ap-northeast-2";
-		var bucketName = Environment.GetEnvironmentVariable("BF_BUCKET", target);
+		AccessKey = Environment.GetEnvironmentVariable("BF_ACCESS_KEY", target);
+		AccessSecret = Environment.GetEnvironmentVariable("BF_ACCESS_SECRET", target);
+		RegionName = Environment.GetEnvironmentVariable("BF_REGION", target) ?? "ap-northeast-2";
+		BucketName = Environment.GetEnvironmentVariable("BF_BUCKET", target);
 
-		if (string.IsNullOrEmpty(appKey) || string.IsNullOrEmpty(appSecret))
+		if (string.IsNullOrEmpty(AccessKey) || string.IsNullOrEmpty(AccessSecret))
 		{
 			Console.WriteLine("BF_ACCESS_KEY or BF_ACCESS_SECRET not found");
 			Environment.Exit(-1);
 		}
-		if (string.IsNullOrEmpty(bucketName))
+		if (string.IsNullOrEmpty(BucketName))
 		{
 			Console.WriteLine("BF_BUCKET not found");
 			Environment.Exit(-1);
 		}
 
-		BuildsFlight.Init(appKey, appSecret, region, bucketName);
+		BuildsFlight.Init(AccessKey, AccessSecret, RegionName, BucketName);
 	}
 }
